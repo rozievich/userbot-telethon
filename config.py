eda4c188f2549e387f9ddd355ba44389eea6b3fe
rozievich <blackcoder459@outlook.com>
2024-08-31 13:54:24 +0500

    Update code by rozievich

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ load_dotenv(os.path.join(".env"))
 api_id = str(os.getenv("API_ID"))
 api_hash = str(os.getenv("API_HASH"))
 bot_token = str(os.getenv("BOT_TOKEN"))
+filter_words = ["salom", "assalomu alaykum", "qanday"]  # Bu yerga kerakli so'zlarni kiriting
+main_group_id = -1002173502304
--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-from telethon.sync import TelegramClient
-from config import api_hash, api_id
+from telethon.sync import TelegramClient, events
+from config import api_hash, api_id, filter_words, main_group_id
 
 
-async def send_message_to_user(client, user_id, message):
-    await client.send_message(user_id, message)
 
-
-async def send_message_to_all_users(client, users, message):
-    for user in users:
-        await client.send_message(user, message)
+async def forward_message(client, target_group, message, user_name, user_telegram, user_phone):
+    formatted_message = f"Xabar: {message.text}\nUserning ismi: {user_name}\nTelegram Username: {user_telegram}\nTelefon raqam: {user_phone or 'Noma’lum'}"
+    await client.send_message(target_group, formatted_message)
 
 
 async def main():
-    client = TelegramClient('send_message_data', api_id, api_hash)
+    client = TelegramClient('taxi_bot', api_id, api_hash)
 
     async with client:
-        user_ids = [6066967779, -1002078072933]
-        for user_id in user_ids:
-            await send_message_to_user(client, user_id, "Assalomu alaykum!")
-        await send_message_to_all_users(client, user_ids, "Hamma foydalanuvchilarga salom!")
+        @client.on(events.NewMessage(chats=await client.get_dialogs()))
+        async def handler(event):
+            if event.is_group and event.chat_id != main_group_id:
+                message_text = event.message.message.lower()  # Xabar matnini kichik harflarga aylantirish
 
+                # Xabar ichida filter so'zlar mavjudligini tekshirish
+                if any(word in message_text for word in filter_words):
+                    # Foydalanuvchi haqida ma'lumot olish
+                    sender = await event.get_sender()
+                    user_name = sender.first_name
+                    user_telegram = f"@{sender.username}" if sender.username else "Noma'lum"
+                    user_phone = sender.phone
+
+                    # Xabarni asosiy guruhga forward qilish
+                    await forward_message(client, main_group_id, event.message, user_name, user_telegram, user_phone)
+
+        print("Bot ishlamoqda...")
+        await client.run_until_disconnected()
 
 if __name__ == '__main__':
     import asyncio
