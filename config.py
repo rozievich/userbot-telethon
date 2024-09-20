fd027d7a26e02e7db7c2f9e02d1b11df08709af3
Rozievich <blackcoder459@outlook.com>
2024-09-21 00:38:21 +0500

    Update code by rozievich

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ load_dotenv(os.path.join(".env"))
 
 api_id = str(os.getenv("API_ID"))
 api_hash = str(os.getenv("API_HASH"))
-bot_token = str(os.getenv("BOT_TOKEN"))
-filter_words = ("kia", "hunday", "yuk", "isuzi", "gazel", "sprintir", "12 ta viloyat", "qoʻyasiz", "uzum", "shoftoli", "oʻrik", "mol", "qoʻy", "goʻsht", "marojna", "sitrus", "qoʻshimcha", "bosring", "ot", "toy", "oʻtin", "avareniy", "laseti", "koblt", "nexia", "katta", "isuzu", "jiguli", "olma")
+filter_words = ("kia", "hunday", "yuk", "isuzi", "gazel", "sprintir", "12 ta viloyat", "qoʻyasiz", "uzum", "shoftoli", "oʻrik", "mol", "qoʻy", "goʻsht", "marojna", "sitrus", "qoʻshimcha", "bosring", "ot", "toy", "oʻtin", "avareniy", "laseti", "koblt", "nexia", "katta", "isuzu", "jiguli", "olma", "Юк", "юк", "Anor", "Olma", "Узум")
 
-main_group_id = -4546126791
+main_group_id = -1002431469752
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import asyncio
+import logging
 from telethon.sync import TelegramClient, events
 from config import api_hash, api_id, filter_words, main_group_id
 
@@ -5,7 +8,10 @@ from config import api_hash, api_id, filter_words, main_group_id
 
 async def forward_message(client, target_group, message, user_name, user_telegram, user_phone, group):
     formatted_message = f"<b>Xabar:</b> {message.text}\n\n<b>Ismi:</b> {user_name}\n<b>Username:</b> {user_telegram}\n<b>Telefon raqam:</b> {user_phone or 'Noma’lum'}\n<b>Guruh:</b> {group}"
-    await client.send_message(target_group, formatted_message, parse_mode='html')
+    try:
+        await client.send_message(target_group, formatted_message, parse_mode='html')
+    except Exception as e:
+        print(e)
 
 
 async def main():
@@ -14,25 +20,30 @@ async def main():
     async with client:
         @client.on(events.NewMessage(chats=await client.get_dialogs()))
         async def handler(event):
-            if event.is_group and event.chat_id != main_group_id:
-                message_text = event.message.message.lower()  # Xabar matnini kichik harflarga aylantirish
+            try:
+                if event.is_group and event.chat_id != main_group_id:
+                    message_text = event.message.message.lower()  # Xabar matnini kichik harflarga aylantirish
 
-                # Xabar ichida filter so'zlar mavjudligini tekshirish
-                if any(word in message_text.lower() for word in filter_words):
-                    # Foydalanuvchi haqida ma'lumot olish
-                    sender = await event.get_sender()
-                    user_name = f'<a href="tg://user?id={sender.id}">{sender.first_name}</a>'
-                    user_telegram = f"@{sender.username}" if sender.username else "Noma'lum"
-                    user_phone = sender.phone
-                    group = f"@{event.chat.username}"
+                    # Xabar ichida filter so'zlar mavjudligini tekshirish
+                    if any(word in message_text.lower() for word in filter_words):
+                        # Foydalanuvchi haqida ma'lumot olish
+                        sender = await event.get_sender()
+                        user_name = f'<a href="tg://user?id={sender.id}">{sender.first_name}</a>'
+                        user_telegram = f"@{sender.username}" if sender.username else "Noma'lum"
+                        user_phone = sender.phone
+                        group = f"@{event.chat.username}"
 
-                    # Xabarni asosiy guruhga forward qilish
-                    await forward_message(client, main_group_id, event.message, user_name, user_telegram, user_phone, group)
-                    await asyncio.sleep(0.033)
+                        # Xabarni asosiy guruhga forward qilish
+                        await forward_message(client, main_group_id, event.message, user_name, user_telegram, user_phone, group)
+                        await asyncio.sleep(0.033)
+            except Exception as e:
+                print("Main function error: ", e)
 
-        print("Bot ishlamoqda...")
+
         await client.run_until_disconnected()
 
 if __name__ == '__main__':
+    # Barcha loglarni chiqarib borish uchun
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     # Asinxron funksiyani ishga tushirish
-    client.loop.run_until_complete(main())
+    asyncio.run(main())
