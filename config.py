d5ac3b32abfcb2dada4f5a848a0867475accf776
Rozievich <blackcoder459@outlook.com>
2024-09-21 19:53:42 +0500

    Update code by rozievich

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(".env"))
 
 api_id = str(os.getenv("API_ID"))
 api_hash = str(os.getenv("API_HASH"))
-filter_words = ("kia", "hunday", "yuk", "isuzi", "gazel", "sprintir", "12 ta viloyat", "qoʻyasiz", "uzum", "shoftoli", "oʻrik", "mol", "qoʻy", "goʻsht", "marojna", "sitrus", "qoʻshimcha", "bosring", "ot", "toy", "oʻtin", "avareniy", "laseti", "koblt", "nexia", "katta", "isuzu", "jiguli", "olma", "Юк", "юк", "Anor", "Olma", "Узум")
-block_list = ("fura", "Фура", "фура", "yuk olamiz", "юк оламиз", "Юк оламиз")
+filter_words = ("kia", "hunday", "yuk", "isuzi", "gazel", "sprintir", "12 ta viloyat", "qoʻyasiz", "uzum", "shoftoli", "oʻrik", "mol", "qoʻy", "goʻsht", "marojna", "sitrus", "qoʻshimcha", "bosring", "ot", "toy", "oʻtin", "avareniy", "laseti", "koblt", "nexia", "katta", "isuzu", "jiguli", "olma", "юк", "Anor", "Olma", "узум", "damas", "дамас")
+block_list = ("fura", "фура", "yuk olamiz", "юк оламиз", "tent", "тент")
 
 main_group_id = -1002431469752
---
+++ test.py
@@ -0,0 +1,2 @@
+data = "узум"
+print(data.upper())
